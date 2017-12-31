02405c2d5091f9f01503cb1125a2e40a76278bf8
Dante Pasionek <dante.pasionek1@gmail.com>
2017-12-31 14:29:56 -0700

    Updated readme, removed all of PythonRandom uses, replaced with Time randomness for seeding

--- PictoRand.py
+++ PictoRand.py
@@ -1,4 +1,4 @@
-import os, sys, wget, tweepy, json, random
+import os, sys, wget, tweepy, json, datetime
 import numpy as np
 from tweepy import OAuthHandler
 from tweepy import models as m
@@ -6,7 +6,7 @@ from PIL import Image
 from io import BytesIO
     
 def initTwitter():
-    #REDACTED
+    #[REDACTED - Twitter Authentication]
     @classmethod
     def parse(cls, api, raw):
         status = cls.first_parse(api, raw)
@@ -24,10 +24,13 @@ def initTwitter():
     api = tweepy.API(auth)
     return api
 
-def getImage():
+
+def getImage(offset=0):
+    now = datetime.datetime.now()
     twitter = initTwitter()
-    users = ['realDonaldTrump', 'NYCDailyPics','dpasi314','TractorPictures']
-    tweets = twitter.user_timeline(screen_name=random.choice(users), count=500, include_rts=False,exclude_replies=True)
+    tags = ['#maga','#art','#weather', '#nofilter']
+    #tweets = twitter.user_timeline(screen_name=random.choice(users), count=500, include_rts=False,exclude_replies=False)
+    tweets = tweepy.Cursor(twitter.search, q=(tags[now.second % len(tags)])).items(50)
     media_files = []
     for status in tweets:
         media = status.entities.get('media', [])
@@ -36,7 +39,8 @@ def getImage():
     if(len(media_files) < 1):
         print("ERROR: Not enough images...")
         return -1
-    return random.choice(media_files)
+    if(offset > 499): offset=499
+    return media_files
 
 def clean_image_file():
     fileList = [f for f in  os.listdir("images/")]
@@ -44,33 +48,39 @@ def clean_image_file():
         print("Removing: ", f)
         os.remove(os.path.join("images/", f))
 
-def getImageFromFile():
+def getImageFromFile(offset=0):
+    now = datetime.datetime.now()
     fileList = [f for f in os.listdir("images/")]
-    return random.choice(fileList)
+    return fileList[now.microsecond % len(fileList)]
 
 def dwnld():
     clean_image_file()
     image = getImage()
-    wget.download(image, out="images/")
+    for i in image:
+        wget.download(i, out="images/")
 
-def generateBitslices():
+def generateBitslices(offset=0):
     image = Image.open("images/" + getImageFromFile())
     image = image.convert('1')
-    image.save("images/" + getImageFromFile())
 
     bitmap = np.array(image).flatten().astype(int)
-    bitslice = np.array_split(bitmap, 50);
-    random.shuffle(bitslice)
+    bitslice = np.array_split(bitmap, 1000);
     return bitslice
 
-def randGen(bitslice, size=8, seed=2, mod=10):
-    mainslice = bitslice[0]
-    for item in bitslice[1:]:
+def randGen(bitslice, size=8, seed=2, mod=10, offset=0):
+    now = bin(int(datetime.datetime.now().strftime("%y%m%d%H%M%S%f%y%m%d%H%M%S%f%y%m%d%H%M%S%f%y%m%d%H%M%S%f")))
+    now = ''.join(str(now).strip("0b"))
+    nowBin = []
+    for item in now: nowBin.append(item)
+    nowBin = np.array(nowBin).flatten().astype(int)
+
+    mainslice = nowBin
+    for item in bitslice: 
         if(len(item) == len(mainslice)): mainslice = mainslice ^ item 
-    
+
     hexslice = [mainslice[start:start+size] for start in range(0, len(mainslice), size)]
-    random.shuffle(hexslice)
     string_list, num_list = [],[]
+
 
     for indiv in hexslice:
         tmp = ''.join([str(s) for s in indiv])
@@ -78,18 +88,23 @@ def randGen(bitslice, size=8, seed=2, mod=10):
     
     for item in string_list:
         num_list.append(int(item, 2))
+
     return np.sum(num_list[::seed]) % mod
 
 
 def main():
     dwnld() # Get all images
-    nums = []
+    sys.setrecursionlimit(1000)
     bitslice = generateBitslices()
-    for i in range(500):
-        nums.append(randGen(bitslice, mod=500))
-        if(i % 100): bitslice = generateBitslices()
-    print()
-    print(nums)
+    n = randGen(bitslice, mod=100)
+    if(False):
+        nums = []
+        for i in range(1000):
+            nums.append(randGen(bitslice, mod=500))
+            if(i % 200):
+                bitslice = generateBitslices()
+        print(nums)
+    print("\n\nRand Number Generated: ",n )
     return 0;
     
 if __name__ == "__main__":
--- ReadMe.md
+++ ReadMe.md
@@ -1,3 +1,21 @@
 # PictoRand
-### A way to generate random numbers based on pictures from twitter
+
+## An Overview
+PictoRand is a way to generate random numbers based on pictures taken from twitter.
+PictoRand will select a picture from the first 50 tweets taken from the following hashtags.
+- MAGA
+- nofilter
+- art
+- weather
+
+PictoRand will take a picture found on twitter, and convert it to black and white. It will then take each of those pixels, associated as a binary number (0, 1) and create a flattened bitmap. 
+A binary string of the current time, expanaded on itself in order to meet size requirements is also created. 
+The time string, which is the main slice, will be XORed over 1,000 similarly sized slices out of the flattened bitmap
+
+## The Inspiration
+While on a quest to search for something truly random, PictoRand was meant to select pictures from twitter to use the thoughts from humans as something truly random. In an attempt to re-create the lava lamp wall that CloudFlare uses to create unique hashes based on pictures.
+
+## The Proof is in the Numbers
 See the math work out here: www.dantepasionek.com/stats.html
+I was able to prove that my random number generator and Python's random number generator are not actually statistically different.
+
