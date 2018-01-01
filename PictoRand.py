699610cc276571d3f2374df76213bbb5c9f4433c
Dante Pasionek <dante.pasionek1@gmail.com>
2018-01-01 15:12:38 -0700

    Cleaned up code/dependencies

--- PictoRand.py
+++ PictoRand.py
@@ -1,12 +1,10 @@
-import os, sys, wget, tweepy, json, datetime
+import os, wget, tweepy, json, datetime
 import numpy as np
 from tweepy import OAuthHandler
 from tweepy import models as m
 from PIL import Image
-from io import BytesIO
-    
+DEBUG = False
 def initTwitter():
-    #[REDACTED - Twitter Authentication]
     @classmethod
     def parse(cls, api, raw):
         status = cls.first_parse(api, raw)
@@ -24,13 +22,14 @@ def initTwitter():
     api = tweepy.API(auth)
     return api
 
-
-def getImage(offset=0):
+def getNormalizedRand(offset):
     now = datetime.datetime.now()
+    return now.microsecond % offset
+
+def getImage():
     twitter = initTwitter()
     tags = ['#maga','#art','#weather', '#nofilter']
-    #tweets = twitter.user_timeline(screen_name=random.choice(users), count=500, include_rts=False,exclude_replies=False)
-    tweets = tweepy.Cursor(twitter.search, q=(tags[now.second % len(tags)])).items(50)
+    tweets = tweepy.Cursor(twitter.search, q=(tags[getNormalizedRand(len(tags))])).items(25)
     media_files = []
     for status in tweets:
         media = status.entities.get('media', [])
@@ -39,36 +38,36 @@ def getImage(offset=0):
     if(len(media_files) < 1):
         print("ERROR: Not enough images...")
         return -1
-    if(offset > 499): offset=499
     return media_files
 
 def clean_image_file():
     fileList = [f for f in  os.listdir("images/")]
     for f in fileList:
-        print("Removing: ", f)
+        if(DEBUG): print("Removing: ", f)
         os.remove(os.path.join("images/", f))
 
-def getImageFromFile(offset=0):
+def getImageFromFile():
     now = datetime.datetime.now()
     fileList = [f for f in os.listdir("images/")]
-    return fileList[now.microsecond % len(fileList)]
+    return fileList[getNormalizedRand(len(fileList))]
 
 def dwnld():
     clean_image_file()
     image = getImage()
     for i in image:
+        if(DEBUG): print("\nDownloading:{}".format(i) )
         wget.download(i, out="images/")
 
 def generateBitslices(offset=0):
-    image = Image.open("images/" + getImageFromFile())
-    image = image.convert('1')
+    image = Image.open("images/" + getImageFromFile()).convert('1')
 
     bitmap = np.array(image).flatten().astype(int)
     bitslice = np.array_split(bitmap, 1000);
     return bitslice
 
 def randGen(bitslice, size=8, seed=2, mod=10, offset=0):
-    now = bin(int(datetime.datetime.now().strftime("%y%m%d%H%M%S%f%y%m%d%H%M%S%f%y%m%d%H%M%S%f%y%m%d%H%M%S%f")))
+    time_format =''.join(["%y%H%f%S%M%y%f" for s in range(50)])
+    now = bin(int(datetime.datetime.now().strftime(time_format)))
     now = ''.join(str(now).strip("0b"))
     nowBin = []
     for item in now: nowBin.append(item)
@@ -79,35 +78,21 @@ def randGen(bitslice, size=8, seed=2, mod=10, offset=0):
         if(len(item) == len(mainslice)): mainslice = mainslice ^ item 
 
     hexslice = [mainslice[start:start+size] for start in range(0, len(mainslice), size)]
-    string_list, num_list = [],[]
-
+    num_list = []
 
     for indiv in hexslice:
         tmp = ''.join([str(s) for s in indiv])
-        string_list.append(tmp)
+        num_list.append(int(tmp, 2))
     
-    for item in string_list:
-        num_list.append(int(item, 2))
-
-    return np.sum(num_list[::seed]) % mod
+    return (np.sum(num_list[::seed]) % mod) + 1
 
 
 def main():
     dwnld() # Get all images
-    sys.setrecursionlimit(1000)
-    bitslice = generateBitslices()
-    n = randGen(bitslice, mod=100)
-    if(False):
-        nums = []
-        for i in range(1000):
-            nums.append(randGen(bitslice, mod=500))
-            if(i % 200):
-                bitslice = generateBitslices()
-        print(nums)
-    print("\n\nRand Number Generated: ",n )
+    n = randGen(generateBitslices(), mod=500)
+    print("\n---PictoRang Generation ---\nGenerated: {}".format(n))
+    clean_image_file()
     return 0;
     
 if __name__ == "__main__":
     main()
-
-
